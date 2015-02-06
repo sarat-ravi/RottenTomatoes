7b7120da941be02b11856f2ce5b76c9a9cce7c2c
Sarat Tallamraju <sarattallamraju@gmail.com>
2015-02-05 21:54:12 -0800

    Initial Commit

---
+++ Rotten Tomatoes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Rotten Tomatoes
+//
+//  Created by Sarat Tallamraju on 2/5/15.
+//  Copyright (c) 2015 Sarat Tallamraju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Rotten Tomatoes/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Rotten Tomatoes
+//
+//  Created by Sarat Tallamraju on 2/5/15.
+//  Copyright (c) 2015 Sarat Tallamraju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
